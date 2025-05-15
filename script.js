2771faea0f6585c6a0c13ff56cb13d36a810a2dc
hawevine <64678375+hawevine@users.noreply.github.com>
2025-05-14 17:47:18 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+// This function adds the task if the condition is met
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Tiene que escribir su informacion!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+
+}, false);
+// This function saves the data
+function saveData(){ 
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+// This function shows the task back each time the browser is closed
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
